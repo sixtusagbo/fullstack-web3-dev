cbd52027594e769dc2968650c9abcebbbe0b9497
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-13 06:18:30 +0100

    Refactor sfStore and sfGet in StorageFactory

--- storage-factory/contracts/StorageFactory.sol
+++ storage-factory/contracts/StorageFactory.sol
@@ -15,12 +15,10 @@ contract StorageFactory {
         // To interact with another SMC, we need the:
         // Address
         // ABI - Application Binary Interface
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        simpleStorage.store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
