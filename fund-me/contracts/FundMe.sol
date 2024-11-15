358a8b36471a3cf3c29a5fea4073e82d4cbf6f91
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-15 02:16:37 +0100

    Validate minimum amount a user can fund

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -8,11 +8,11 @@ pragma solidity ^0.8.8;
 import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18;
 
     function fund() public payable {
         // Set a minimum fund amount in USD
-        require(msg.value >= minimumUsd, "Didn't send enough"); // 1e18 = 1 * 10^18 // in wei
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough"); // 1e18 = 1 * 10^18 // in wei
     }
 
     function getPrice() public view returns (uint256) {
@@ -30,8 +30,10 @@ contract FundMe {
         return priceFeed.version();
     }
 
-    function getConversionRate() public {
-
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 
     // function withdraw() {}
