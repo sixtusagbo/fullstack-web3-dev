1d8ac4ff7c6c20e3da241705af32a9010391db00
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-15 01:44:16 +0100

    Update FundMe.sol

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -1,11 +1,11 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
 contract FundMe {
-    // Get funds from users
-    // Withdraw funds
-    // Set a minimum funding value in USD
-
     function fund() public payable {
         // Set a minimum fund amount in USD
         require(msg.value >= 1e18, "Didn't send enough"); // 1e18 = 1 * 10^18 // in wei
