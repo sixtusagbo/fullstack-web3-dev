a7e050baac98122b2ba5e5e2beca098b9d05f412
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-15 01:38:20 +0100

    Learn about setting default value with require

---
+++ fund-me/contracts/FundMe.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract FundMe {
+    // Get funds from users
+    // Withdraw funds
+    // Set a minimum funding value in USD
+
+    function fund() public payable {
+        // Set a minimum fund amount in USD
+        require(msg.value >= 1e18, "Didn't send enough"); // 1e18 = 1 * 10^18 // in wei
+    }
+
+    // function withdraw() {}
+}
