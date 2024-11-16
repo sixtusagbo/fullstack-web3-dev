1c97f19428d695bab2e0582ffeb1c9d63f25eae6
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-16 20:43:32 +0100

    Create SafeMathTester.sol
    Learn about checked and unchecked in solidity math

---
+++ fund-me/contracts/SafeMathTester.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SafeMathTester {
+    uint8 public bigNumber = 255;
+
+    function add() public {
+        bigNumber = bigNumber + 1; // Openzeppelin SafeMath.sol was used to prevent this from wrapping
+    }
+}
