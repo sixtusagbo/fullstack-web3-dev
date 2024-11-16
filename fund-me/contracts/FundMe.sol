5b306f1e0cc1a7e7eb478bb0b10c3453ee277f07
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-16 20:20:48 +0100

    Refactor FundMe contract with a library

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -5,35 +5,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
         // Set a minimum fund amount in USD
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough"); // 1e18 = 1 * 10^18 // in wei
-    }
-
-    function getPrice() public view returns (uint256) {
-        // ABI
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // Price of ETH in terms of USD
-        // 3000.00000000
-        return uint256(price * 1e10); // 1^10 
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); // 1e18 = 1 * 10^18 in wei
+        funders.push(msg.sender);
     }
 
     // function withdraw() {}
