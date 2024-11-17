7a420152060e93cdcfafd4f0994295b90fc122b4
Sixtus Miracle Agbo <miracleagbosixtus@gmail.com>
2024-11-17 03:24:18 +0100

    Implement withdraw functionality

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -15,11 +15,39 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         // Set a minimum fund amount in USD
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); // 1e18 = 1 * 10^18 in wei
         funders.push(msg.sender);
     }
 
-    // function withdraw() {}
+    function withdraw() public {
+        require(msg.sender == owner, "Only contract owner is allowed to withdraw!");
+        // for loop
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the funders array
+        funders = new address[](0);
+
+        // actually withdraw
+
+        // // transfer
+        // // msg.sender is of type address
+        // // while payable(msg.sender) is casted to type payable address
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSucess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSucess, "Call failed");
+    }
 }
